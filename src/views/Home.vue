8dc8a5cec033af6db2bf39232feea94bc872b01a
Diogo da Silva França Marins <dmarins@braspag.com.br>
2019-09-22 19:49:20 -0300

    construindo a home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+    <produtos-buscar></produtos-buscar>
+    <produtos-lista></produtos-lista>
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista
+  }
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
